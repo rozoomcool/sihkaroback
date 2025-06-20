d3d2fbe3e433cadb33472d8cf9524c5a8cca01b0
rozoomcool <rozoom.um@gmail.com>
2025-06-20 06:20:43 +0300

    simple example

--- cmd/sihkaro/main.go
+++ cmd/sihkaro/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin:     func(r *http.Request) bool { return true },
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade: ", err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read message: ", err)
+			break
+		}
+		log.Println("Recieved: ", string(message))
+		conn.WriteMessage(messageType, message)
+	}
+}
 
 func main() {
-	fmt.Println("Hello world")
+	http.HandleFunc("/ws", wsHandler)
+	log.Println("Server started on :8000")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
